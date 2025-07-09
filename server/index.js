27bc3a98d4cac768ff0cdc34c1951f9c0f3b3732
Tsegaye Kebede <tdrag301@gmail.com>
2025-07-10 01:31:23 +0300

    add some improvement in the backend index

--- server/index.js
+++ server/index.js
@@ -10,17 +10,19 @@ dotenv.config({ path: path.resolve(__dirname, 'config.env') });
 // Initialize Express app
 const app = express();
 const allowedOrigins = [
-  'https://my-portfolio-bfgg.onrender.com' // frontend
+  'https://my-portfolio-bfgg.onrender.com'
 ];
+
 
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
-
-// Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -43,21 +45,12 @@ app.use('/api/auth', authRoutes);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve(); // add this line if using ESM
-  app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
 }
-
-
-  // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
-
 
 // Connect to MongoDB
 const connectDB = async () => {
